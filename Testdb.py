7194fe11d6644bb80655ad22593952b22c764517
Lohith <lohithkc7@gmail.com>
2022-08-02 21:12:18 +0530

    This is 1st file to git push

---
+++ Testdb.py
@@ -0,0 +1,16 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb://lohith369:Mongo369@ac-qn6cw65-shard-00-00.onq988i.mongodb.net:27017,ac-qn6cw65-shard-00-01.onq988i.mongodb.net:27017,ac-qn6cw65-shard-00-02.onq988i.mongodb.net:27017/?ssl=true&replicaSet=atlas-vkjq5c-shard-0&authSource=admin&retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "Name" : "Lohith",
+    "Age" : 30,
+    "Email" : "lohith@gmail.com",
+    "Address" : "bangalore"
+}
+
+db1 = client['mongggtest']
+collection = db1['test']
+collection.insert_one(d)
