4becbdda4919a6ee3d684c5631e8ff0567a738bc
Lohith <lohithkc7@gmail.com>
2022-08-09 20:13:48 +0530

    Monogo db script

---
+++ test2.py
@@ -0,0 +1,5 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb://lohith369:Mongo369@ac-qn6cw65-shard-00-00.onq988i.mongodb.net:27017,ac-qn6cw65-shard-00-01.onq988i.mongodb.net:27017,ac-qn6cw65-shard-00-02.onq988i.mongodb.net:27017/?ssl=true&replicaSet=atlas-vkjq5c-shard-0&authSource=admin&retryWrites=true&w=majority")
+db = client.test
+print(db)
